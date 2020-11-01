5813c03da21c09091af771c3e3bff03dc722989d
codeblake <codeblake@outlook.com>
2020-11-01 01:11:23 +0000

    Changed attached direction

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gaps      = 50;       /* gap size */
 static const unsigned int gappih    = gaps;     /* horiz inner gap between windows */
 static const unsigned int gappiv    = gaps;     /* vert inner gap between windows */
-static const unsigned int gappoh    = gaps*2;     /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = gaps*2;     /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = gaps*2;   /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = gaps*2;   /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -40,10 +40,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.465; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;     /* number of clients in master area */
-static const int resizehints = 1;     /* 1 means respect size hints in tiled resizals */
-static const int attachdirection = 0; /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
+static const float mfact         = 0.465; /* factor of master area size [0.05..0.95] */
+static const int nmaster         = 1;     /* number of clients in master area */
+static const int resizehints     = 1;     /* 1 means respect size hints in tiled resizals */
+static const int attachdirection = 5; /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
